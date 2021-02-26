be6495f218a6c7f9765e3adc3127d9290e591700
fabirguez <72743213+fabirguez@users.noreply.github.com>
2021-02-26 02:33:09 +0100

    init login breeze

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,28 +12,13 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/', HomeController::class);
-Route::get('/users', [UserController::class, 'index']);
-Route::get('/users/show/{id}', [UserController::class, 'show']);
-Route::get('/users/create', [UserController::class, 'create']);
 
-// Route::get('/', function () {
-//     // return view('welcome');
-//     return 'Bienvenido página principa';
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
 
-/* Route::get('cursos', function () {
-    // return view('welcome');
-    return 'Bienvenido página cursos';
-}); */
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
-/* Route::get('cursos/create', function () {
-    return 'En esta pagina podrás crear un curso';
-}); */
-// TIENE ORDEN, TENER CUIDADO CON RUTAS Y VARIABLES, ESE ES EL ULTIMO
-
-/* Route::get('cursos/{curso}/{categoria?}', function ($curso, $categoria == null) {
-    // return view('welcome');
-    //Se puede usar ifelse x si se quiere poner una u otra
-    return 'Bienvenido página curso: $curso de la categoria: $categoria';
-}); */
+require __DIR__.'/auth.php';
